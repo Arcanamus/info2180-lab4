5a3d7910315ba889fa88f8cda5e4bef4a9014aaf
Jevon Forrest <jevonforrest77@gmail.com>
2021-10-29 13:07:16 -0500

    Final update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 window.addEventListener('load', function(){
     var searchButton = document.querySelector(".button");
-    var url = 'http://localhost/info2180-lab4/superheroes.php';
-
+    var form = document.querySelector('#searchBox');
+    var url = `superheroes.php?query=${form.value}`;
+    var result = document.querySelector('#result');
 
     searchButton.addEventListener('click', function(event){
         event.preventDefault();
         fetch(url)
         .then(response => response.text())
-        .then(text => alert(text))
+        .then(text => result.innerHTML = text)
         .catch(error => {console.log("Unable to complete this request! Please try again.")});
     });
 });
